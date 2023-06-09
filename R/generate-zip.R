0add8e0d1b5fec6067bfb492802ea7a31529ff7f
Ben Welman <bwelman@protonmail.com>
2023-06-09 12:42:28 +0200

    Change info help. Change name data files.

--- R/generate-zip.R
+++ R/generate-zip.R
@@ -1,3 +1,3 @@
-system("7z u -tzip Practicefiles-LearnAccess.zip -w data/.")
+system("7z u -tzip files-learnaccess.zip -w data/.")
 
 # The zip file is only updated when an existing file changes or add/remove files
